0563405de80bcdd672639530033f93839c2101b5
Mathew Varughese <mat@imginconline.com>
2016-06-19 15:59:13 -0400

    $digest dirty checks

--- src/scope.js
+++ src/scope.js
@@ -1,3 +1,5 @@
+function initWatchVal() { }
+
 function Scope() {
     this.$$watchers = [];
 }
@@ -5,13 +7,23 @@ function Scope() {
 Scope.prototype.$watch = function(watchFn, listenerFn) {
     var watcher = {
         watchFn: watchFn,
-        listenerFn: listenerFn
+        listenerFn: listenerFn,
+        last: initWatchVal
     };
     this.$$watchers.push(watcher);
 };
 
 Scope.prototype.$digest = function() {
+    var self = this;
+    var newValue, oldValue;
     _.forEach(this.$$watchers, function(watcher) {
-        watcher.listenerFn();
+        newValue = watcher.watchFn(self);
+        oldValue = watcher.last;
+        if(oldValue !== newValue) {
+            watcher.last = newValue;
+            watcher.listenerFn(newValue,
+                (oldValue === initWatchVal) ? newValue : oldValue,
+                self);
+        }
     });
 };
